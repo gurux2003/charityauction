c3d779735ef865104aed965dcfd6b59436e71ea5
gurux2003 <gurux437@gmail.com>
2025-05-22 10:23:47 +0530

    hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,41 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { PrivateKey } = require("./secret.json"); // Make sure secret.json exports { "PrivateKey": "yourkey" }
+
 module.exports = {
-  solidity: "0.8.28",
+  defaultNetwork: "testnet",
+
+  networks: {
+    hardhat: {},
+    testnet: {
+      url: "https://rpc.test2.btcs.network",
+      accounts: [''], // Using your key from secret.json
+      chainId: 1114,
+    },
+  },
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.24",
+        settings: {
+          evmVersion: "shanghai",
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+
+  paths: {
+    sources: "./contracts",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+
+  mocha: {
+    timeout: 20000,
+  },
 };
